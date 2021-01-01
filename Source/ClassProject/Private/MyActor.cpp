b13f60edb07c949e0fd555e7e0b58758da2edf95
Smartuil <563412673@qq.com>
2021-01-01 21:50:25 +0800

    基础框架搭建完成👀

---
+++ Source/ClassProject/Private/MyActor.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	A = 200;
+	B = 1.0f;
+}
+
+void AMyActor::MyFunc()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Hello World"));
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ClassProject/Public/MyActor.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class CLASSPROJECT_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Damage")
+	int32 A;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category="Damage")
+	float B;
+
+	UFUNCTION(BlueprintCallable, Category="Damage")
+	void MyFunc();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
